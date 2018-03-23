90720670c18d7f319ec14d9779e100702c8d22ee
Christophe <christophe.demarey@inria.fr>
2018-03-23 15:42:20 +0100

    teporarly disable option for nsis

--- windows/build-windows-installer.sh
+++ windows/build-windows-installer.sh
@@ -3,8 +3,9 @@
 VERSION=${VERSION:-"1.0.0"}
 
 # NSIS is run in the directory where the NSI file stands
-cp pharo-launcher.ico windows/
+cp icons/pharo-launcher.ico windows/
 mkdir windows/Pharo-win
 mv Pharo windows/Pharo-win/
-pharo-build-scripts/windows-installer/NSIS/Bin/makensis.exe /DVERSION=$VERSION windows/pharo-launcher-installer-builder.nsi
+# pharo-build-scripts/windows-installer/NSIS/Bin/makensis.exe /DVERSION=$VERSION windows/pharo-launcher-installer-builder.nsi
+pharo-build-scripts/windows-installer/NSIS/Bin/makensis.exe windows/pharo-launcher-installer-builder.nsi
 mv windows/pharo-launcher-installer.exe pharo-launcher-installer-"$VERSION".exe

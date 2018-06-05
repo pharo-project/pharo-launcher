7cda1cc9af39c76683004a876010dfb46705e72b
Christophe <christophe.demarey@inria.fr>
2018-06-05 13:30:05 +0200

    dedicated run script for Pharo Launcher

--- build.sh
+++ build.sh
@@ -68,7 +68,12 @@ function package_user_version() {
 
 function package_linux_version() {
 	set_env
-	./pharo-build-scripts/build-platform.sh -i Pharo -o Pharo -r $PHARO -s $PHARO_SOURCES -v $VERSION-$DATE -t Pharo -p linux
+	rm -f pharo-build-scripts/platform/icons/*
+	cp icons/pharo-launcher.png pharo-build-scripts/platform/icons/
+	rm pharo-build-scripts/platform/templates/linux/%\{NAME\}.template
+	cp linux/pharo-launcher.template pharo-build-scripts/platform/templates/linux/
+	./pharo-build-scripts/build-platform.sh -i Pharo -o PharoLauncher -r $PHARO -s $PHARO_SOURCES -v $VERSION-$DATE -t PharoLauncher -p linux
+
 	mv Pharo-linux.zip PharoLauncher-linux-$VERSION_NUMBER.zip
 }
 

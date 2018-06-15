b7ed0a401e1974042402b0b4e500f4472a9d2f09
Christophe <christophe.demarey@inria.fr>
2018-06-15 11:57:29 +0200

    fix executable name so that chmod succeeds and add missing arg to set_pharo_sources_version

--- build.sh
+++ build.sh
@@ -72,7 +72,7 @@ function package_linux_version() {
 	cp icons/pharo-launcher.png pharo-build-scripts/platform/icons/
 	rm pharo-build-scripts/platform/templates/linux/%\{NAME\}.template
 	cp linux/pharo-launcher pharo-build-scripts/platform/templates/linux/pharo-launcher.template
-	./pharo-build-scripts/build-platform.sh -i PharoLauncher -o PharoLauncher -r $PHARO -s $PHARO_SOURCES -v $VERSION-$DATE -t PharoLauncher -p linux
+	EXECUTABLE_NAME=pharo-launcher ./pharo-build-scripts/build-platform.sh -i Pharo -o PharoLauncher -r $PHARO -s $PHARO_SOURCES -v $VERSION-$DATE -t PharoLauncher -p linux
 
 	mv PharoLauncher-linux.zip PharoLauncher-linux-$VERSION_NUMBER.zip
 }
@@ -161,7 +161,7 @@ function set_pharo_sources_version() {
 }
 
 function get_pharo_sources_version() {
-	set_pharo_sources_version
+	set_pharo_sources_version ${PHARO_SOURCES}
 	wget --quiet https://files.pharo.org/sources/PharoV${PHARO_SOURCES}.sources.zip && unzip PharoV${PHARO_SOURCES}.sources.zip PharoV${PHARO_SOURCES}.sources && rm PharoV${PHARO_SOURCES}.sources.zip
 }
 

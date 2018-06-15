37483f8c56b86f2ebf6abcf2b47ca2e9ab8fa495
Christophe <christophe.demarey@inria.fr>
2018-06-15 14:20:07 +0200

    set workspace to avoid to get wrong architecure image

--- build.sh
+++ build.sh
@@ -72,7 +72,7 @@ function package_linux_version() {
 	cp icons/pharo-launcher.png pharo-build-scripts/platform/icons/
 	rm pharo-build-scripts/platform/templates/linux/%\{NAME\}.template
 	cp linux/pharo-launcher pharo-build-scripts/platform/templates/linux/pharo-launcher.template
-	EXECUTABLE_NAME=pharo-launcher ./pharo-build-scripts/build-platform.sh -i Pharo -o PharoLauncher -r $PHARO -s $PHARO_SOURCES -v $VERSION-$DATE -t PharoLauncher -p linux
+	EXECUTABLE_NAME=pharo-launcher WORKSPACE=$(pwd)/One ./pharo-build-scripts/build-platform.sh -i Pharo -o PharoLauncher -r $PHARO -s $PHARO_SOURCES -v $VERSION-$DATE -t PharoLauncher -p linux
 
 	mv PharoLauncher-linux.zip PharoLauncher-linux-$VERSION_NUMBER.zip
 }

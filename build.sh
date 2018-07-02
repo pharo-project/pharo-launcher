b61218c44aa1bdacbe66351fc6c93dd1d4d31c7f
Guille Polito <guillermopolito@gmail.com>
2018-07-02 16:50:43 +0200

    fix repository directory

--- build.sh
+++ build.sh
@@ -28,7 +28,7 @@ function prepare_image() {
 
 	./pharo Pharo.image save PharoLauncher --delete-old
 	./pharo PharoLauncher.image --version > version.txt
-	./pharo PharoLauncher.image eval --save "Metacello new baseline: 'PharoLauncher'; repository: 'gitlocal://..'; load"
+	./pharo PharoLauncher.image eval --save "Metacello new baseline: 'PharoLauncher'; repository: 'gitlocal://src'; load"
 }
 
 function run_tests() {

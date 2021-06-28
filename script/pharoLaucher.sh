36cf2ac9109436bf081eb2da27adddb0ecfb9556
Christophe Demarey <christophe.demarey@inria.fr>
2021-06-28 11:22:21 +0200

    add pharo-launcher cmd line scripts to linux and mac package

--- script/pharoLaucher.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# this is MacOS batch file to shortcut call of vm executable with image argument with clap parameter
-../../vms/80-x64/Pharo.app/Contents/MacOS/Pharo --headless *.image clap launcher $@

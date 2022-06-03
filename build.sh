dfadffa088094dbedacde2f302a9f05fc8b157df
Christophe Demarey <christophe.demarey@inria.fr>
2022-06-03 11:34:26 +0200

    ensure there are no launcher files before testing

--- build.sh
+++ build.sh
@@ -32,6 +32,7 @@ function prepare_image() {
 }
 
 function run_tests() {
+	rm -rf ~/Pharo # clean posssible remaining Pharo files
 	./pharo PharoLauncher.image test --junit-xml-output "PharoLauncher.*"	
 }
 
